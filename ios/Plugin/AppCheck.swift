b2dea06e3a6176908c8ad60f75b515ca3a33ca49
Matt <matt@devleaf.studio>
2021-08-04 23:36:33 +0100

    Resolves issues with iOS plugin not working

--- ios/Plugin/AppCheck.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-@objc public class AppCheck: NSObject {
-    @objc public func echo(_ value: String) -> String {
-        return value
-    }
-}
---
+++ ios/Plugin/AppCheckProviderFactory.swift
@@ -0,0 +1,11 @@
+import Firebase
+
+class CustomAppCheckProviderFactory: NSObject, AppCheckProviderFactory {
+    func createProvider(with app: FirebaseApp) -> AppCheckProvider? {
+        if #available(iOS 14.0, *) {
+            return AppAttestProvider(app: app);
+        } else {
+            return DeviceCheckProvider(app: app);
+        }
+    }
+}
