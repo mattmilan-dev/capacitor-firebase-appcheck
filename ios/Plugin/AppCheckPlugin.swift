11ac4986594c581f55be1c37218a4dcb8c527def
Matt <email@mattmilan.dev>
2021-08-05 18:53:20 +0100

    Resolves issue with iOS

--- ios/Plugin/AppCheckPlugin.swift
+++ ios/Plugin/AppCheckPlugin.swift
@@ -3,6 +3,17 @@ import Capacitor
 import FirebaseCore
 import FirebaseAppCheck
 
+class CustomAppCheckProviderFactory: NSObject, AppCheckProviderFactory {
+    func createProvider(with app: FirebaseApp) -> AppCheckProvider? {
+        if #available(iOS 14.0, *) {
+            return AppAttestProvider(app: app);
+        } else {
+            return DeviceCheckProvider(app: app);
+        }
+    }
+}
+
+
 @objc(AppcheckPlugin)
 public class AppcheckPlugin: CAPPlugin {
     @objc func initialize(_ call: CAPPluginCall) {
